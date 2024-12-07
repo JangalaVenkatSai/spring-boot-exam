df20b76f53ff40887c9135a775a15659d3a391b6
Venkat Sai Jangala <141983339+JangalaVenkatSai@users.noreply.github.com>
2024-12-07 10:56:01 +0530

    Add files via upload

---
+++ jfsdendsemlab/src/main/java/com/venky/first/JfsdendsemlabApplication.java
@@ -0,0 +1,13 @@
+package com.venky.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsdendsemlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfsdendsemlabApplication.class, args);
+	}
+
+}
